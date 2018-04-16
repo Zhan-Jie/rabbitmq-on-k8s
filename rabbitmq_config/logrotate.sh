8b6d23b54057d087197229964d10f59c4fb27108
zhanjie <916952976@qq.com>
2018-04-16 17:12:07 +0800

    remove expired log files

--- rabbitmq_config/logrotate.sh
+++ rabbitmq_config/logrotate.sh
@@ -10,3 +10,6 @@ log_bak="/var/log/rabbitmq/${RABBITMQ_NODENAME}.log$log_suffix"
 my_ip=$(hostname -i)
 mv $log_bak "/var/log/rabbitmq/${my_ip}_${MY_POD_NAME}_${cur_time}.log"
 rm -vf "/var/log/rabbitmq/${RABBITMQ_NODENAME}-sasl.log$log_suffix"
+
+# delete files that are last modified 7 days ago
+find /var/log/rabbitmq/ -mtime +7 -type f | xargs rm -vf
